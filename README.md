dc353719dfb1a89dfeda4c9c0c593afbfe6d38c1
Shanosha <wootenm1@gmail.com>
2026-01-27 16:45:15 -0500

    added project files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Refelctions
+
+Why is it important to handle errors for each individual API call rather than just at the end of the promise chain?
+
+- You can thorw custom error classes that way.
+
+How does using custom error classes improve debugging and error identification?
+
+- It's easier for you to identify the error reason.
+
+When might a retry mechanism be more effective than an immediate failure response?
+
+- If there was a timeout issue where the API call was waiting for a response for too long.
---
+++ apiSimulator.js
@@ -0,0 +1,94 @@
+class NetworkError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "NetworkError";
+    }
+}
+
+class DataError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "DataError";
+    }
+}
+
+export const fetchProductCatalog = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                if (Math.random() < 0.8) {
+                    resolve([
+                        { id: 1, name: "Laptop", price: 1200 },
+                        { id: 2, name: "Headphones", price: 200 },
+                    ]);
+                } else {
+                    throw new DataError("Failed to fetch product catalog");
+                }
+            } catch (e) {
+                reject(e);
+            }
+        }, 1500);
+    });
+};
+
+export const fetchProductReviews = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            try {
+                const reviews = [
+                    { productId: 1, content: "I love this product", rating: 5 },
+                    { productId: 1, content: "I hate this product", rating: 1 },
+                    { productId: 1, content: "Awesome product I love it", rating: 5 },
+                ];
+                const currentId = reviews[0].productId;
+                if (Math.random() < 0.8) {
+                    resolve(reviews);
+                } else {
+                    throw new NetworkError(
+                        `Failed to fetch reviews for product ID ${currentId}`,
+                    );
+                }
+            } catch (e) {
+                reject(e);
+            }
+        }, 1500);
+    });
+};
+
+export const fetchSalesReport = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const salesReport = { totalSales: 500, unitsSold: 5, averagePrice: 100 };
+            if (Math.random() < 0.8) {
+                resolve(salesReport);
+            } else {
+                reject("Failed to fetch sales report");
+            }
+        }, 1000);
+    });
+};
+
+fetchProductCatalog()
+    .then((product) => {
+        console.log("Fetched Products:", product);
+        return fetchProductReviews(); // returns a promise object
+    })
+    .then((reviews) => {
+        console.log("Fetched Reviews:", reviews);
+        return fetchSalesReport(); // returns a promise object
+    })
+    .then((sales) => {
+        console.log("Fetched Sales:", sales);
+    })
+    .catch((e) => {
+        if (e instanceof DataError) {
+            console.error("dataError", e);
+        } else if (e instanceof NetworkError) {
+            console.error("NetworkError", e);
+        } else {
+            console.error(e)
+        }
+    })
+    .finally(() => {
+        console.log("API Calls Completed");
+    });
